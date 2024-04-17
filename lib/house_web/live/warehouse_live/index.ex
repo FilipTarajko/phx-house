7dcd9bd533c7ec1f89c3d689756502f739cbd295
ft <filiptarajko@gmail.com>
2024-04-17 03:34:39 +0200

    Disable editing/deleting warehouses on /warehouses page

--- lib/house_web/live/warehouse_live/index.ex
+++ lib/house_web/live/warehouse_live/index.ex
@@ -14,12 +14,6 @@ defmodule HouseWeb.WarehouseLive.Index do
     {:noreply, apply_action(socket, socket.assigns.live_action, params)}
   end
 
-  defp apply_action(socket, :edit, %{"id" => id}) do
-    socket
-    |> assign(:page_title, "Edit Warehouse")
-    |> assign(:warehouse, Warehouses.get_warehouse!(id))
-  end
-
   defp apply_action(socket, :new, _params) do
     socket
     |> assign(:page_title, "New Warehouse")
@@ -35,13 +29,5 @@ defmodule HouseWeb.WarehouseLive.Index do
   @impl true
   def handle_info({HouseWeb.WarehouseLive.FormComponent, {:saved, warehouse}}, socket) do
     {:noreply, stream_insert(socket, :warehouses, warehouse)}
-  end
-
-  @impl true
-  def handle_event("delete", %{"id" => id}, socket) do
-    warehouse = Warehouses.get_warehouse!(id)
-    {:ok, _} = Warehouses.delete_warehouse(warehouse)
-
-    {:noreply, stream_delete(socket, :warehouses, warehouse)}
   end
 end
