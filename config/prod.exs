f7674630ff4fbc92a2e6ba7039e792bc2bf80c41
ft <filiptarajko@gmail.com>
2024-05-04 05:10:10 +0200

    Disable swoosh in prod config

--- config/prod.exs
+++ config/prod.exs
@@ -10,10 +10,11 @@ config :house, HouseWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Configures Swoosh API Client
-config :swoosh, api_client: Swoosh.ApiClient.Finch, finch_name: House.Finch
+# config :swoosh, api_client: Swoosh.ApiClient.Finch, finch_name: House.Finch
+config :swoosh, :api_client, false
 
 # Disable Swoosh Local Memory Storage
-config :swoosh, local: false
+# config :swoosh, local: false
 
 # Do not print debug messages in production
 config :logger, level: :info
