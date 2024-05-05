a6313343d768fd4610b7636eb96a1284b2753d59
ft <filiptarajko@gmail.com>
2024-05-05 04:41:50 +0200

    Rename breadcrumb/warehouse data loading module to PreparePathData

--- lib/house_web/prepare_path_data.ex
+++ lib/house_web/prepare_path_data.ex
@@ -1,20 +1,20 @@
-defmodule HouseWeb.PrepareBreadcrumData do
-  def on_mount(:save_request_uri, _params, _session, socket) do
+defmodule HouseWeb.PreparePathData do
+  def on_mount(:prepare_path_data, _params, _session, socket) do
     {:cont,
       Phoenix.LiveView.attach_hook(
         socket,
-        :prepare_breadcrumb_and_warehouse_data,
+        :prepare_path_data,
         :handle_params,
-        &prepare_breadcrumb_and_warehouse_data/3
+        &prepare_path_data/3
       )
     }
   end
 
-  defp prepare_breadcrumb_and_warehouse_data(params, url, socket) do
+  defp prepare_path_data(params, url, socket) do
     uri = URI.parse(url) |> Map.get(:path)
     uri_with_substitutions = uri
 
-    {uri_with_substitutions, socket} = prepare_warehouse_id_substitution(uri_with_substitutions, socket, params)
+    {uri_with_substitutions, socket} = prepare_warehouse_data(uri_with_substitutions, socket, params)
 
     uri_splits = String.split(uri, "/") |> Enum.drop(1)
     uri_with_substitutions_splits = String.split(uri_with_substitutions, "/") |> Enum.drop(1)
@@ -39,7 +39,7 @@ defmodule HouseWeb.PrepareBreadcrumData do
     |> Enum.reverse()
   end
 
-  defp prepare_warehouse_id_substitution(uri_with_substitutions, socket, params) do
+  defp prepare_warehouse_data(uri_with_substitutions, socket, params) do
     if (Map.has_key?(params, "warehouse_id")) do
       name = House.Warehouses.get_warehouse!(params["warehouse_id"]).name
       IO.puts("warehouse name: #{name}")
